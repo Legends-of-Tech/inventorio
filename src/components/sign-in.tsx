6ccff8629c909a83a23b9370f1519c4056f7c34d
AidenTran15 <aidenkiettran@gmail.com>
2023-02-19 23:11:17 +1100

    fixed JSX.IntrinicElements error

--- src/components/sign-in.tsx
+++ src/components/sign-in.tsx
@@ -1,6 +1,6 @@
 import { LockClosedIcon } from '@heroicons/react/20/solid'
 
-const signin = () => {
+const Signin = () => {
   return (
     <>
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -92,4 +92,4 @@ const signin = () => {
   )
 }
 
-export default signin;
+export default Signin;
