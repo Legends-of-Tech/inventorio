9051708a265490d972474d8dc032cb61b66fb5f7
AidenTran15 <aidenkiettran@gmail.com>
2023-03-24 17:13:23 +1100

    added the total order section

--- src/pages/CreateOrderPage.tsx
+++ src/pages/CreateOrderPage.tsx
@@ -15,36 +15,69 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 const CreateOrderPage = () => {
   return (
     <div className="min-h-full">
-      <header className="top-section py-3 px-12 w-full max-w-screen-lg flex items-center justify-center border-b border-black">
-        <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-          Tạo Đơn Hàng
-        </h1>
-      </header>
-      <div className="fixed top-40 left-0 right-0 z-10 flex justify-center">
-        <Link to="/customers-page">
-          <Fab
-            variant="extended"
-            size="large"
-            sx={{
-              backgroundColor: "white",
-              color: "#90A4AE",
-              border: "1.5px solid #90A4AE"
-            }}
-          >
-            <AccountCircleIcon sx={{ mr: 1, color: "#90A4AE", fontSize: "2rem" }} />
-            <span
-              style={{
-                fontSize: "1.2rem",
-                fontFamily: "Arial, sans-serif",
-                fontWeight: 500,
-                textTransform: "capitalize",
-              }}>
-              Chọn Khách Hàng
-            </span>
-          </Fab>
-        </Link>
+      <div className="content flex flex-col min-h-screen">
+        <header className="top-section py-3 px-12 w-full max-w-screen-lg flex items-center justify-center border-b border-black">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+            Tạo Đơn Hàng
+          </h1>
+        </header>
+        <div className="fixed top-40 left-0 right-0 z-10 flex justify-center">
+          <Link to="/customers-page">
+            <Fab
+              variant="extended"
+              size="large"
+              sx={{
+                backgroundColor: "white",
+                color: "#90A4AE",
+                border: "1.5px solid #90A4AE"
+              }}
+            >
+              <AccountCircleIcon sx={{ mr: 1, color: "#90A4AE", fontSize: "2rem" }} />
+              <span
+                style={{
+                  fontSize: "1.2rem",
+                  fontFamily: "Arial, sans-serif",
+                  fontWeight: 500,
+                  textTransform: "capitalize",
+                }}>
+                Chọn Khách Hàng
+              </span>
+            </Fab>
+          </Link>
+        </div>
       </div>
-    </div>
+
+      <div className="sticky-bottom">
+
+        <div className="flex items-center justify-between w-full mb-2.5">
+          <div className="flex items-center ml-5">
+            <ShoppingBasketIcon />
+            <div className='flex items-center ml-3 mt-1'>
+              <h1 className="text-lg font-bold">TỔNG ĐƠN</h1>
+            </div>
+          </div>
+          <div className="flex mr-8">
+            <span className="rounded-l-md border border-r-0 border-gray-300 px-3 text-gray-500 sm:text-sm">
+              0
+            </span>
+            <span className="border border-r-0 border-gray-300 px-4 text-gray-500 sm:text-sm">
+              0đ
+            </span>
+          </div>    
+        </div>
+
+
+        <div className="w-full bg-white border-t border-black py-2.5 flex items-center justify-center">
+            <Button variant="contained" startIcon={<ShoppingCartRounded />}>
+            Hoàn Tất Đơn Hàng
+            </Button>
+        </div>
+      </div>
+
+
+
+      </div>
+
 
 
 
