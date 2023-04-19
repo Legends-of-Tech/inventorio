62bf44be83397203db4e2f4cb447902876527797
erayus <raymondt@pageuppeople.com>
2023-04-20 00:53:32 +1000

    Reorganize pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import BottomNavBar from './components/NavBar/BottomNavBar';
 import TopNavBar from './components/NavBar/TopNavBar';
 import ProductListPage from './pages/InventoryPage/InventoryPage';
 import OrderPage from './pages/OrderPage/OrderPage';
 import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
-import CreateOrderPage from './pages/CreateOrderPage/CreateOrderPage';
+import CreateOrderPage from './pages/OrderPage/CreateOrderPage/CreateOrderPage';
 import AddNewCustomer from './pages/AddNewCustomer/AddNewCustomer';
-
 
 Amplify.configure(awsconfig);
 
@@ -18,10 +17,11 @@ function App() {
               <TopNavBar header={"Đơn Hàng"} />
               <main className="main mb-auto pt-3" style={{marginTop: '6rem'}}>
                   <Routes>
-                      <Route path="/" element={<OrderPage />} />
+                      <Route path="/orders/create-order-page" element={<CreateOrderPage />} />
+                      <Route path="/orders" element={<OrderPage />} />
                       <Route path="/all-products" element={<ProductListPage />} />
                       <Route path="/add-new-customer" element={<AddNewCustomer />} />
-                      <Route path="/create-order-page" element={<CreateOrderPage />} />
+                      <Route path="*" element={<Navigate to="/orders" replace />} />
                   </Routes>
               </main>
               <BottomNavBar className="fixed inset-x-0 bottom-0 h-16 right-0 vw-100" />
--- src/pages/OrderPage/OrderPage.tsx
+++ src/pages/OrderPage/OrderPage.tsx
@@ -1,6 +1,6 @@
 import { Fab} from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-import { useNavigate } from 'react-router-dom';
+import {  useNavigate } from 'react-router-dom';
 
 function OrderPage() {
   const navigate = useNavigate();
@@ -8,7 +8,7 @@ function OrderPage() {
   return (
       <>
           <div className="flex justify-center">
-              <Fab className='mt-5' variant="extended" size="large" color="primary" onClick={() => navigate("/create-order-page")}>
+              <Fab className='mt-5' variant="extended" size="large" color="primary" onClick={() => navigate("create-order-page")}>
                   <AddIcon sx={{ mr: 1 }} />
                   Tạo Đơn Hàng
               </Fab>
